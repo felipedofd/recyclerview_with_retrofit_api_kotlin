8d955d8911142d9b2f598cf38fd5d50cc67100c5
felipedofd <felipedofd@hotmail.com>
2021-11-18 17:03:42 -0300

    add files

---
+++ app/src/test/java/com/example/recyclerview_with_retrofit_api_kotlin/DogApiResponseUnitTest.kt
@@ -0,0 +1,17 @@
+package com.example.recyclerview_with_retrofit_api_kotlin
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class DogApiResponseUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
